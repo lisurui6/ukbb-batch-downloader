ce0355b0f73cb55551a9b8f1cae6c7b977e471d2
lisurui6 <lisurui6@gmail.com>
2022-08-23 15:29:49 +0100

    change master input options

--- master.py
+++ master.py
@@ -19,8 +19,10 @@ CWD = Path(__file__).parent
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--input-dir", dest="input_dir", type=str,
-                        help="input dir expects to have subdirectories ./csv ./utils ./key")
+    parser.add_argument("--csv-file", dest="csv_file", type=str, required=True,
+                        help="List of EIDs to download, column name eid")
+    parser.add_argument("--key-path", dest="key_path", type=str, required=True)
+    parser.add_argument("--ukbfetch-path", dest="ukbfetch_path", type=str, required=True)
     parser.add_argument("--n-partition", dest="n_partition", type=int)
     parser.add_argument("--output-dir", dest="output_dir", type=str)
     parser.add_argument("--n-thread", dest="n_thread", type=int, default=0)
@@ -36,7 +38,7 @@ def partition_csv(csv_path: Path, n_partition: int, output_dir: Path):
     return dfs
 
 
-def create_batch_files(input_dir: Path, part_csv_files: List[Path], output_dir: Path, n_thread) -> List[Path]:
+def create_batch_files(key_path: Path, ukbfetch_path: Path, part_csv_files: List[Path], output_dir: Path, n_thread) -> List[Path]:
     job_script_path = CWD.joinpath("job.py")
     python_command = f"python {str(job_script_path)}"
     with open(str(CWD.joinpath("batch_template.txt")), "r") as file:
@@ -45,7 +47,8 @@ def create_batch_files(input_dir: Path, part_csv_files: List[Path], output_dir: 
     temp_batch_file_dir.mkdir(exist_ok=True, parents=True)
     batch_files = []
     for idx, csv_file in enumerate(part_csv_files):
-        command = python_command + f" --input-dir {str(input_dir)} --csv-file {str(csv_file)} --output-dir {str(output_dir)}  --n-thread {n_thread}"
+        command = python_command + f" --key-path {str(key_path)} --ukbfetch-path {str(ukbfetch_path)} " \
+                                   f"--csv-file {str(csv_file)} --output-dir {str(output_dir)}  --n-thread {n_thread}"
         batch = sbatch.format(idx + 1) + f"{command}\n"
         batch_file = temp_batch_file_dir.joinpath("")
         with open(f"job_{idx}.sh", "r") as file:
@@ -61,12 +64,15 @@ def submit_batch_files(batch_files: List[Path]):
 
 def main():
     args = parse_args()
-    input_dir = Path(args.input_dir)
+    csv_file = Path(args.csv_file)
+    key_path = Path(args.key_path)
+    ukbfetch_path = Path(args.ukbfetch_path)
+
     output_dir = Path(args.output_dir)
     n_partition = args.n_partition
-    csv_file = input_dir.joinpath("csv", "ukbb_40616_new_eids.csv")
+
     part_csv_files = partition_csv(csv_file, n_partition, output_dir.joinpath("temp", "csv"))
-    batch_files = create_batch_files(input_dir, part_csv_files, output_dir, args.n_thread)
+    batch_files = create_batch_files(key_path, ukbfetch_path, part_csv_files, output_dir, args.n_thread)
     submit_batch_files(batch_files)
 
 
